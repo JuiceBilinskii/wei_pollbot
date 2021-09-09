a2dc9d4a55524de10a1111f68795a0368855f021
bilin <bilinskij2001@gmail.com>
2021-09-09 21:19:42 +0300

    Added base handler

---
+++ handlers.py
@@ -0,0 +1,11 @@
+import aiogram
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import CommandStart
+from load_all import dp, db
+
+@dp.message_handler(CommandStart())
+async def register_user(message: types.Message, state: FSMContext):
+    # db.update_user(message.chat.id, 1)
+    # await state.update_data({'number_of_replies': 1})
+    await message.answer(f'Echobot yibatb')
