f4d1c32928a745f279f87010425b7f8083af191a
bilin <bilinskij2001@gmail.com>
2021-09-11 01:30:34 +0300

    Added base functionality for poll

--- handlers.py
+++ handlers.py
@@ -5,13 +5,11 @@ from aiogram.dispatcher.filters import CommandStart
 from load_all import dp, db
 from itertools import combinations
 from states import Poll
-from inline_keyboards import create_character_choice
+from inline_keyboards import create_character_choice, create_ratio_choice, create_empty
 import random
 
 @dp.message_handler(CommandStart())
 async def register_user(message: types.Message, state: FSMContext):
-    # db.update_user(message.chat.id, 1)
-    # await state.update_data({'number_of_replies': 1})
     await message.answer(f'Этот бот предназначен для проведения опроса, результаты которого будут задействованы в дальнейшем анализе.')
 
 
@@ -22,9 +20,52 @@ async def start_poll(message: types.Message, state: FSMContext):
     random.shuffle(character_combinations)
 
     await state.update_data({'characters_combinations': character_combinations})
-    await state.update_data({'current_choice': 0})
+    await state.update_data({'current_question': 0})
+    await state.update_data({'total_questions': len(character_combinations)})
 
     await message.answer('Опрос начат')
-    await message.answer(f'{character_combinations[0][0][1]} - {character_combinations[0][0][2]}\n{character_combinations[0][1][1]} - {character_combinations[0][1][2]}',
-    reply_markup=create_character_choice(character_combinations[0][0][1], character_combinations[0][1][1]))
+
+    text, keyboard = await create_question(state)
+    await message.answer(text, reply_markup=keyboard)
+
     await Poll.Polling.set()
+    
+
+@dp.callback_query_handler(text='left', state=Poll.Polling)
+async def process_left_character(query: types.CallbackQuery):
+    message = query.message
+    await message.edit_reply_markup(reply_markup=create_ratio_choice())
+
+@dp.callback_query_handler(text='right', state=Poll.Polling)
+async def process_right_character(query: types.CallbackQuery):
+    message = query.message
+    await message.edit_reply_markup(reply_markup=create_empty())
+
+@dp.callback_query_handler(text='3', state=Poll.Polling)
+@dp.callback_query_handler(text='5', state=Poll.Polling)
+@dp.callback_query_handler(text='7', state=Poll.Polling)
+@dp.callback_query_handler(text='9', state=Poll.Polling)
+async def send_question(query: types.CallbackQuery, state: FSMContext):
+
+    data = await state.get_data()
+    question_number = data.get('current_question')
+
+    message = query.message
+    if question_number < data.get('total_questions'):
+        text, keyboard = await create_question(state)
+
+        await message.answer(text, reply_markup=keyboard)
+    else:
+        await message.answer('Опрос окончен')
+
+async def create_question(state: FSMContext):
+    data = await state.get_data()
+
+    question_number = data.get('current_question')
+    character_a, character_b = data.get('characters_combinations')[question_number]
+
+    text = f"{character_a[1]} - {character_a[2]}\n{character_a[3]}\n\n{character_b[1]} - {character_b[2]}\n{character_b[3]}"
+    keyboard = create_character_choice(character_a[1], character_b[1])
+
+    await state.update_data({'current_question': question_number + 1})
+    return text, keyboard
--- inline_keyboards.py
+++ inline_keyboards.py
@@ -3,9 +3,9 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def create_character_choice(first_character, second_character):
     names_and_data = (
-        (first_character, 'A'),
+        (first_character, 'left'),
         ('Равны', '1'),
-        (second_character, 'B'),
+        (second_character, 'right'),
     )
 
     row_buttons = (InlineKeyboardButton(text, callback_data=data) for text, data in names_and_data)
@@ -13,12 +13,14 @@ def create_character_choice(first_character, second_character):
     character_choice = InlineKeyboardMarkup(inline_keyboard=[[*row_buttons]])
     return character_choice
 
-choose_ratio = InlineKeyboardMarkup(
-    inline_keyboard=[
-        InlineKeyboardButton('1'),
-        InlineKeyboardButton('3'),
-        InlineKeyboardButton('5'),
-        InlineKeyboardButton('7'),
-        InlineKeyboardButton('9'),
-    ]
-)
+def create_ratio_choice():
+    ratios = ('3', '5', '7', '9')
+
+    row_buttons = (InlineKeyboardButton(ratio, callback_data=ratio) for ratio in ratios)
+
+    ratio_choice = InlineKeyboardMarkup(inline_keyboard=[[*row_buttons]])
+    return ratio_choice
+
+def create_empty():
+    ratio_choice = InlineKeyboardMarkup()
+    return ratio_choice
