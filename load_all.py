6809b7f61daa237fa236ea711f06f967919df418
bilin <bilinskij2001@gmail.com>
2021-10-01 20:24:49 +0300

    Preparations to deploy on Heroku

--- load_all.py
+++ load_all.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from database import DatabaseQueries
+import os
 import configparser
 
 
@@ -9,8 +10,13 @@ storage = MemoryStorage()
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-bot = Bot(token=config['Bot-config']['API_TOKEN'])
+# bot = Bot(token=config['Bot-config']['API_TOKEN'])
+bot = Bot(token=os.environ['API_TOKEN'])
 dp = Dispatcher(bot, storage=storage)
 
 db_config = config['PostgreSQL']
-db = DatabaseQueries(db_config['host'], db_config['database'], db_config['user'], db_config['password'])
+# db = DatabaseQueries(db_config['host'], db_config['database'], db_config['user'], db_config['password'])
+db = DatabaseQueries(host=os.environ['host'],
+                     database=os.environ['database'],
+                     user=os.environ['user'],
+                     password=os.environ['password'])
