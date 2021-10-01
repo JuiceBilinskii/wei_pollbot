4fd884a71dfa4ed4397795222049012dceb5b75e
bilin <bilinskij2001@gmail.com>
2021-10-02 01:54:22 +0300

    Changed accessing to environment variables

--- load_all.py
+++ load_all.py
@@ -7,16 +7,16 @@ import configparser
 
 storage = MemoryStorage()
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+# config = configparser.ConfigParser()
+# config.read('config.ini')
 
 # bot = Bot(token=config['Bot-config']['API_TOKEN'])
-bot = Bot(token=os.environ['API_TOKEN'])
+bot = Bot(token=os.environ.get('API_TOKEN'))
 dp = Dispatcher(bot, storage=storage)
 
-db_config = config['PostgreSQL']
+# db_config = config['PostgreSQL']
 # db = DatabaseQueries(db_config['host'], db_config['database'], db_config['user'], db_config['password'])
-db = DatabaseQueries(host=os.environ['host'],
-                     database=os.environ['database'],
-                     user=os.environ['user'],
-                     password=os.environ['password'])
+db = DatabaseQueries(host=os.environ.get('host'),
+                     database=os.environ.get('database'),
+                     user=os.environ.get('user'),
+                     password=os.environ.get('password'))
